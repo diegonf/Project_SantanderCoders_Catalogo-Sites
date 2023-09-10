1a9f68922370ad6ee17bf4d46dc104b0ea297c42
Diego Ferreira <diego.nascimento91@gmail.com>
2023-09-10 19:34:06 -0300

    feito chamada da inizialização do form no index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { Catalogo } from "./Catalogo.js";
+import { Form } from "./Form.js";
 
 Catalogo.cadastrarCatalogoInicial_excluir();
 Catalogo.carregarCatalogoInicial();
+
+Form.inicializarEventos();
+
